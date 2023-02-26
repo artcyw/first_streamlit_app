a3234dca423e1b27568a1d2079e4fbbe0bb83de8
Albert Wong <a.wongcy@gmail.com>
2023-02-25 22:47:28 -0800

    Update streamlit_app.py
    add Fruit Advice header
    add response json

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
 
