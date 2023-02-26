c243e1130a06e3008b49b6bfe51781812ffec72a
Albert Wong <a.wongcy@gmail.com>
2023-02-26 12:30:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,8 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
 
-streamlit.dataframe(my_data_row)
 
 streamlit.stop()
 
