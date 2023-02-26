0e19637c4eca913d46134dd788734eef395f9c7d
Albert Wong <a.wongcy@gmail.com>
2023-02-26 12:17:11 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 import snowflake.connector
 
