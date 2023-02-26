7ac4a6e929048ce7f26ce51fda37775285da1a17
Albert Wong <a.wongcy@gmail.com>
2023-02-25 22:49:43 -0800

    Update streamlit_app.py
    change get to kiwi
    normalize response to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 import pandas as pd
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) # normalize json response and put into pandas table
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -27,5 +27,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
 
